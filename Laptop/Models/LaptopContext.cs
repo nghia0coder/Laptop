185a0f15f0c1ec05ac4c982261636a4e4fde278e
MinhTam <buivominhtam123@gmail.com>
2024-03-06 13:31:46 +0700

    c1

--- Laptop/Models/LaptopContext.cs
+++ Laptop/Models/LaptopContext.cs
@@ -36,7 +36,7 @@ namespace GiayDep.Models
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
 
-                optionsBuilder.UseSqlServer("Data Source=NGHIANGHIA\\SQLSEVER2020EV;Initial Catalog=QLLAPTOP;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+                optionsBuilder.UseSqlServer("Data Source=MTAM\\SQLSERVER2022DEV;Initial Catalog=QLLAPTOP;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
             }
         }
